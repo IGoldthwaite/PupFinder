8fe9c85d04783ed264347ccb95b9bd8a06a25658
Nick Driscoll <nick.a.mavros@gmail.com>
2016-11-19 21:53:55 -0500

    Added picture button and error handling

--- app/src/main/java/com/thelittlestpetshop/pupfinder/MapsActivity.java
+++ app/src/main/java/com/thelittlestpetshop/pupfinder/MapsActivity.java
@@ -1,7 +1,14 @@
 package com.thelittlestpetshop.pupfinder;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +17,26 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final String hostName = "";
+    static final int portNumber = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +46,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //Button doggoPictureButton = (Button)findViewById(R.id.doggoPictureButton);
     }
 
 
@@ -42,5 +68,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public void doggoPictureTaker(View view) {
+        //Attempt server connection
+        Socket echoSocket = null;
+        try {
+            echoSocket = new Socket(hostName, portNumber);
+        } catch (Exception ex) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Couldn't connect to server!", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException io) {
+
+        }
+
+        /*
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.example.android.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+        */
+
+        //Upload photo to server
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out = null;
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(photoFile);
+            out.flush();
+            byte[] yourBytes = bos.toByteArray();
+
+        } catch (IOException io) {
+            // ignore exceptions
+        } finally {
+            try {
+                bos.close();
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
+
+
+        try {
+            PrintWriter outWriter =
+                    new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn =
+                    new BufferedReader(
+                            new InputStreamReader(System.in));
+        } catch (Exception ex) {
+
+        }
+
     }
 }
